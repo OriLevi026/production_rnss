991a7b40f15787af54dafba2f013b1ea11c1c6a7
OriLevi <orilevi026@gmail.com>
2019-01-03 10:23:27 +0200

    Updates

---
+++ precache-manifest.b459cbadfa454a5ce92d69284a199ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "906f76ec9e81f118efed",
+    "url": "/renew_senses_web/static/js/runtime~main.906f76ec.js"
+  },
+  {
+    "revision": "301046d09185b3712522",
+    "url": "/renew_senses_web/static/js/main.301046d0.chunk.js"
+  },
+  {
+    "revision": "4ac259c963a59cffdbc0",
+    "url": "/renew_senses_web/static/js/1.4ac259c9.chunk.js"
+  },
+  {
+    "revision": "301046d09185b3712522",
+    "url": "/renew_senses_web/static/css/main.57f4cf09.chunk.css"
+  },
+  {
+    "revision": "f37513b7fe703a2e51c303035b376bad",
+    "url": "/renew_senses_web/index.html"
+  }
+];
